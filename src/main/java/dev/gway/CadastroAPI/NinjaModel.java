7c6f61577b9bd6ee3175f3a26b474c635623d811
Gabriel Lima <Gabriellimma00@gmail.com>
2025-09-02 15:29:35 -0300

    Criar Entidade Cadastro para o abanco de dados

---
+++ src/main/java/dev/gway/CadastroAPI/NinjaModel.java
@@ -0,0 +1,62 @@
+package dev.gway.CadastroAPI;
+
+//Entity transforma uma classe em enitidade no banco de dados
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+
+// JPA = jarkarta persistence API
+
+@Entity
+@Table(name = "tb_cadastro")
+
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+
+    public int getIdade() {
+
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+
+        this.idade = idade;
+    }
+
+}
