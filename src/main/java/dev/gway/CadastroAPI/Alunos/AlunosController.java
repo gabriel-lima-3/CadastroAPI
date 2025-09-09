896142c7f8c5d22dd1d9887f76aaad6e4ce74865
Gabriel Lima <Gabriellimma00@gmail.com>
2025-09-09 16:16:02 -0300

    feat: Atualiza o Controller com Mock de dados

--- src/main/java/dev/gway/CadastroAPI/Alunos/AlunosController.java
+++ src/main/java/dev/gway/CadastroAPI/Alunos/AlunosController.java
@@ -1,9 +1,7 @@
 package dev.gway.CadastroAPI.Alunos;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -14,5 +12,35 @@ public class AlunosController {
         return "Essa e minha primeira mensagem nessa rota";
     }
 
+    //CRUD ---------------------------------------
 
+    // Adicionar Aluno (Create)
+    @PostMapping("/adicionar")
+    public String criarAluno() {
+        return "Ninja criado";
+    }
+
+    //Mostrar todos os Alunos (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosAlunos() {
+        return "Mostrar todos os alunos";
+    }
+
+    //Procurar Aluno por id (READ)
+    @GetMapping("/porId")
+    public String mostrarAlunosPorId() {
+        return "Mostrar  alunos por id";
+    }
+
+    //Alterar dado dos Alunos (Update)
+    @PutMapping("/alterar")
+    public String alterarAlunoPorId() {
+        return "Alterar  alunos por id";
+    }
+
+    //Deletar Aluno (Delete)
+    @DeleteMapping("/deletarId")
+    public String deletarAlunosPorId() {
+        return "Retornar aluno deletado por id";
+    }
 }
