7a78e135a27ed2b8346cf428f758c88209789b40
Gabriel Lima <Gabriellimma00@gmail.com>
2025-09-09 16:30:27 -0300

    feat: Cria as rotas do Controller com mock das Atividades

--- src/main/java/dev/gway/CadastroAPI/Atividades/AtividadesController.java
+++ src/main/java/dev/gway/CadastroAPI/Atividades/AtividadesController.java
@@ -1,17 +1,37 @@
 package dev.gway.CadastroAPI.Atividades;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("atividades")
 public class AtividadesController {
 
-    @GetMapping("/missaoDoDia")
-    public String missoesDoDia(){
-        return "A missoes de hoje";
+// Get - Mandar requisição para mostrar as atividades
+    @GetMapping("/listar")
+    public String listar(){
+        return "Ativades listada com sucesso";
     }
+
+
+// Post - Mandar uma requisição para criar atividades
+    @PostMapping("/criar")
+    public String criarAtividade(){
+
+        return "Atividade criada com sucesso";
+    }
+// Put - Mandar requisição  para alterar a Atividade
+    @PutMapping("/alterar")
+    public String alterarAtividade(){
+        return "Atividade alterada com sucesso";
+    }
+
+// Delete - Mandar requisição para deletar as Atividades
+    @DeleteMapping("/deletar")
+    public String deletarAtividade(){
+        return "Atividade deletada com sucesso";
+    }
+
+
 
 
 }
