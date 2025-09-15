bf99ce53b34da606c99774f5afeae5e54e0cfa6d
Gabriel Lima <Gabriellimma00@gmail.com>
2025-09-15 18:46:42 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente no AlunoMapper os caminhos entidades x DTO

--- src/main/java/dev/gway/CadastroAPI/Alunos/AlunosDTO.java
+++ src/main/java/dev/gway/CadastroAPI/Alunos/AlunosDTO.java
@@ -1,4 +1,28 @@
 package dev.gway.CadastroAPI.Alunos;
+import dev.gway.CadastroAPI.Atividades.AtividadesModel;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlunosDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String curso;
+    private String imagemUrl;
+    private AtividadesModel atividades;
+
+    private String semestre;
+
+
+
+
+
 }
--- src/main/java/dev/gway/CadastroAPI/Alunos/AlunosMapper.java
+++ src/main/java/dev/gway/CadastroAPI/Alunos/AlunosMapper.java
@@ -1,4 +1,41 @@
 package dev.gway.CadastroAPI.Alunos;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AlunosMapper {
+
+    public AlunosModel map (AlunosDTO alunosDTO) {
+
+        AlunosModel alunosModel = new AlunosModel();
+
+        alunosModel.setId(alunosDTO.getId());
+        alunosModel.setNome(alunosDTO.getNome());
+        alunosModel.setEmail(alunosDTO.getEmail());
+        alunosModel.setIdade(alunosDTO.getIdade());
+        alunosModel.setCurso(alunosDTO.getCurso());
+        alunosModel.setImagemUrl(alunosDTO.getImagemUrl());
+        alunosModel.setSemestre(alunosDTO.getSemestre());
+        alunosModel.setAtividades(alunosDTO.getAtividades());
+
+        return alunosModel;
+
+    }
+
+    public AlunosDTO map(AlunosModel alunosModel) {
+
+        AlunosDTO alunosDTO = new AlunosDTO();
+
+        alunosDTO.setId(alunosModel.getId());
+        alunosDTO.setNome(alunosModel.getNome());
+        alunosDTO.setEmail(alunosModel.getEmail());
+        alunosDTO.setIdade(alunosModel.getIdade());
+        alunosDTO.setCurso(alunosModel.getCurso());
+        alunosDTO.setImagemUrl(alunosModel.getImagemUrl());
+        alunosDTO.setSemestre(alunosModel.getSemestre());
+        alunosDTO.setAtividades(alunosModel.getAtividades());
+
+        return alunosDTO;
+    }
+
 }
